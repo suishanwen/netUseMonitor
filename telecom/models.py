d3a50ff9de55c86ab4bb8033621fca13c212f127
suishanwen <suishanwen@icloud.com>
2018-11-23 13:15:50 +0800

    U

--- telecom/models.py
+++ telecom/models.py
@@ -12,5 +12,5 @@ class Card(models.Model):
     user = models.CharField(max_length=40, default='')
     net = models.CharField(max_length=100, default='')
     remark = models.CharField(max_length=100, default='')
-    sort = models.IntegerField(default=None)
+    sort = models.IntegerField(default=None, null=True)
     update = models.DateTimeField(auto_now=True)
