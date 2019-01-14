7d9b78349abd301b4bfde1153d9f8eefe2647d45
suishanwen <suishanwen@icloud.com>
2019-01-14 18:04:29 +0800

    add job

--- netUseMonitor/job.py
+++ netUseMonitor/job.py
@@ -6,6 +6,6 @@ logger = logging.getLogger('django')
 
 def clean():
     now = datetime.datetime.now()
-    min_ago = now - datetime.timedelta(minutes=1)
+    min_ago = now - datetime.timedelta(minutes=5)
     Online.objects.filter(update__lt=min_ago).delete()
     logger.info("delete inactive")
