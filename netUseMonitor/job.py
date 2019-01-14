b92da945ca41e0a3a5e8995d3e57d2a5c55655fd
suishanwen <suishanwen@icloud.com>
2019-01-14 17:44:03 +0800

    add job

--- netUseMonitor/job.py
+++ netUseMonitor/job.py
@@ -1,12 +1,11 @@
-import logging
-from datetime import datetime, timedelta
+import logging,datetime
 from telecom.models import Online
 
 logger = logging.getLogger('django')
 
 
 def clean():
-    now = datetime.now()
-    min_ago_5 = now - timedelta(minutes=5)
+    now = datetime.datetime.now()
+    min_ago_5 = now - datetime.timedelta(minutes=5)
     Online.objects.filter(update__lt=min_ago_5).delete()
     logger.info("delete inactive")
